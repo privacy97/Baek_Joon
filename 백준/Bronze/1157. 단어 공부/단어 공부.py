c8d8d72965aa6e1d4590aa39c32d05bc364d614f
privacy97 <89842901+privacy97@users.noreply.github.com>
2022-08-06 17:27:50 +0900

    [Bronze I] Title: 단어 공부, Time: 264 ms, Memory: 32796 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -0,0 +1,20 @@
+a = str(input())
+
+a = a.upper()
+
+a_dic = {}
+
+for i in a:
+    if i in a_dic:
+        a_dic[i] += 1
+    else:
+        a_dic[i] = 1
+
+a_dic = sorted(a_dic.items(), key=lambda x:x[1], reverse=True)
+
+if len(a_dic) == 1:
+	print(a_dic[0][0])
+elif (a_dic[0][1]) == (a_dic[1][1]):
+    print('?')
+else:
+    print(a_dic[0][0])
