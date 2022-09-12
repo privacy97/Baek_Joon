09a7673b8dfa44dc6f4254ae6c6ff598c3f12cbc
privacy97 <89842901+privacy97@users.noreply.github.com>
2022-09-12 17:25:08 +0900

    [Gold V] Title: Moo 게임, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/5904. Moo 게임/Moo 게임.py
@@ -0,0 +1,50 @@
+def moo(num, result):
+
+    num += 1
+    result = (result*2) + num
+
+    if result >= n:
+        return num, result
+
+    return moo(num, result)
+
+
+n = int(input())
+
+if n < 26:
+    if n == 1 or n == 4 or n == 8 or n == 11 or n == 16 or n == 19 or n == 23:
+        print('m')
+        exit()
+    else:
+        print('o')
+        exit()
+
+num = 3
+result = 3
+
+num, result = moo(num, result)
+
+#print()
+#print(n, num, result)
+
+while True:
+
+    n = n - num - int((result-num)/2)
+    if n < 26:
+        break
+
+    num = 3
+    result = 3
+
+    num, result = moo(num, result)
+
+    #print()
+    #print(n, num, result)
+
+#print()
+#print(n, num, result)
+
+if n == 1 or n == 4 or n == 8 or n == 11 or n == 16 or n == 19 or n == 23:
+    print('m')
+else:
+    print('o')
